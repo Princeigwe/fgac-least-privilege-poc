73b575fb387af4131742684cdd3a16db57360c2e
princeigwe <igwep297@gmail.com>
2026-03-04 16:30:23 +0100

    local db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.NODE_ENV === "production"
+              ? process.env.RDS_HOSTNAME
+              : process.env.NODE_ENV === "staging"
+              ? process.env.STAGING_DB_HOST
+              : process.env.DB_HOST,
+      port:  parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: false,
+      entities: ['**/*.entity.ts'],
+      // migrations: ['src/database/migrations/*-migration.ts'],
+      migrationsRun: false,
+      logging: true,
+      ssl: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging' ? {
+          rejectUnauthorized: false
+        } : false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
