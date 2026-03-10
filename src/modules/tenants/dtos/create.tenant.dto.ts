a3a8b5f99300d18c5cb8ed73f81b9d341e5266a5
princeigwe <igwep297@gmail.com>
2026-03-10 21:55:37 +0100

    docs for tenants endpoints

--- src/modules/tenants/dtos/create.tenant.dto.ts
+++ src/modules/tenants/dtos/create.tenant.dto.ts
@@ -1,14 +1,18 @@
 import { IsString, IsNotEmpty, IsEmail } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateTenantDto{
+  @ApiProperty({description: "The name of the tenant", example: "tenant1"})
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({description: "The admin name of the tenant", example: "admin1"})
   @IsString()
   @IsNotEmpty()
   tenantAdminName: string;
 
+  @ApiProperty({description: "The email address of the tenant admin", example: "admin1@gmail.com"})
   @IsEmail()
   @IsNotEmpty()
   tenantAdminEmail: string;
--- src/modules/tenants/tenants.controller.ts
+++ src/modules/tenants/tenants.controller.ts
@@ -5,7 +5,10 @@ import { JwtAuthGuard } from '../auth/guards/jwt.auth.guard';
 import { FineGrainedPermissionGuard } from '../auth/guards/fine.grained.permission.guard';
 import { RequirePermission } from '../auth/decorators/require.permission.decorator';
 import { Tenant } from './tenant.entity';
+import { ApiTags, ApiBearerAuth, ApiOperation, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('Tenants')
+@ApiBearerAuth()
 @Controller('tenants')
 export class TenantsController {
   constructor(
@@ -13,6 +16,8 @@ export class TenantsController {
   ){}
 
 
+  @ApiOperation({description: "This endpoint creates a tenant together with a tenant admin. This operation can only be carried out by the super-admin. This gives the tenant admin access to all existing resources"})
+  @ApiBody({type: CreateTenantDto})
   @UseGuards(JwtAuthGuard, FineGrainedPermissionGuard)
   @RequirePermission(Tenant.name, 'create')
   @Post()
@@ -24,6 +29,7 @@ export class TenantsController {
     )
   }
 
+  @ApiOperation({description: "This operation gets all registered tenants on the API. Can only be executed by the super-admin."})
   @UseGuards(JwtAuthGuard, FineGrainedPermissionGuard)
   @RequirePermission(Tenant.name, 'read')
   @Get()
