84fa1baaa94066381881d18962bb27aee67b9c62
princeigwe <igwep297@gmail.com>
2026-03-10 21:21:52 +0100

    swagger installation and setup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RequestMethod, ValidationPipe } from '@nestjs/common';
-import * as bodyParser from 'body-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -27,6 +27,17 @@ async function bootstrap() {
     origin: '*',
     credentials: false,
   });
+
+
+  const mainOptions = new DocumentBuilder()
+    .setTitle('FGAC Permissions-System API')
+    .setDescription('Official API documentation for multi-tenant permissions management')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  
+  const mainDocument = SwaggerModule.createDocument(app, mainOptions);
+  SwaggerModule.setup('api-docs', app, mainDocument);
 
   await app.listen(process.env.PORT ?? 3000);
 }
